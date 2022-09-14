9d2deeb45efd72b3a9cafb6805c366fe4caf12a3
brhanegiday <brhane5giday@gmail.com>
2022-09-14 22:59:06 +0300

    Update App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import Home from './pages/home';
+import Home from './pages/Home';
+import Cuisine from './pages/Cuisine';
+import Category from './components/Category';
+import Search from './components/Search';
+import SearchResults from './pages/SearchResults';
+
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='px-5 xl:px-0 container max-w-6xl mx-auto my-12'>
+      <BrowserRouter>
+        <Category />
+        {/* <Search /> */}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/cuisine/:cuisineId' element={<Cuisine />} />
+          <Route path='/search/:query' element={<SearchResults />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
