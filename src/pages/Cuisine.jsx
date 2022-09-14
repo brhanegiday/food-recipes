7867f04e01dd032fa0bfed98b03f5a9f66e3b51c
brhanegiday <brhane5giday@gmail.com>
2022-09-14 22:58:42 +0300

    File restructuring

---
+++ src/pages/Cuisine.jsx
@@ -0,0 +1,77 @@
+import { useState, useEffect, useCallback } from 'react';
+import { Link, useParams } from 'react-router-dom';
+function Cuisine() {
+  const [cuisine, setCuisine] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const { cuisineId } = useParams();
+
+  const getCuisine = useCallback(
+    (cat) => {
+      const check = localStorage.getItem(`cuisine-${cuisineId}`);
+      if (check) {
+        setCuisine(JSON.parse(check));
+        setLoading(false);
+        setError(null);
+      } else {
+        fetch(
+          `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.REACT_APP_API_Key}&cuisine=${cat}`
+        )
+          .then((res) => {
+            if (!res.ok)
+              throw new Error(`This is an HTTP error: ${res.status}`);
+            return res.json();
+          })
+          .then((data) => {
+            localStorage.setItem(
+              `cuisine-${cuisineId}`,
+              JSON.stringify(data.results)
+            );
+            setCuisine(data.results);
+            setError(null);
+            console.log(data.results);
+          })
+          .catch((err) => {
+            setError(err.message);
+            setCuisine(null);
+          })
+          .finally(() => {
+            setLoading(false);
+          });
+      }
+    },
+    [cuisineId]
+  );
+
+  useEffect(() => {
+    getCuisine(cuisineId);
+  }, [cuisineId, getCuisine]);
+
+  return (
+    <div className=''>
+      <h1 className='pb-6 text-2xl'>
+        <span className='capitalize'>{cuisineId}</span> Cuisine
+      </h1>
+      {loading && <div>A moment please...</div>}
+      {error && (
+        <div>{`There is a problem fetching the post data - ${error}`}</div>
+      )}
+      <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-y-6 gap-x-4 w-full mx-auto'>
+        {cuisine &&
+          cuisine.map((recipe) => (
+            <Link to={`cuisine/${cuisineId}`} key={recipe.id}>
+              <img
+                className='w-full object-cover h-52 rounded-lg'
+                src={recipe.image}
+                alt={recipe.title}
+              />
+              <p className='font-bold text-sm py-2'>{recipe.title}</p>
+            </Link>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default Cuisine;
