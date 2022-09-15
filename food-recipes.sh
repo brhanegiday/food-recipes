2fcae58047a27a1e5fd41e24d55a70e981c93bb7
Brhane Giday <35131050+brhanegiday@users.noreply.github.com>
2022-09-15 13:50:23 +0300

    Update food-recipes.sh

--- food-recipes.sh
+++ food-recipes.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Brhane Giday
    # Additional Authors:
-   # <your GitHub username>
+   # brhanegiday
 
    # Input:
    # p, principal amount
